cea36de455e4818c11351e277d86a27a714069be
Devitaraja <yellovedevitaraja0203@mail.ugm.ac.id>
2024-09-02 07:59:04 +0700

    new

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyBv-db0qdg0gYJFKlQFOFIZU3xedanzIRQ",
+  authDomain: "reciclica-app-3ec10.firebaseapp.com",
+  projectId: "reciclica-app-3ec10",
+  storageBucket: "reciclica-app-3ec10.appspot.com",
+  messagingSenderId: "19774387922",
+  appId: "1:19774387922:web:cb5fd4c861813b1bcb1f0b"
+  }
+};
